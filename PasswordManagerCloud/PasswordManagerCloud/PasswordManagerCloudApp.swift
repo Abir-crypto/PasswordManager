40161f32138acd3d7c880e7c0df36979f57e07ec
Abir <abir@Abir-Mac-Mini.local>
2023-05-06 11:06:51 +0600

    swtiched to swiftui

---
+++ PasswordManagerCloud/PasswordManagerCloud/PasswordManagerCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordManagerCloudApp.swift
+//  PasswordManagerCloud
+//
+//  Created by Abir on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PasswordManagerCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
